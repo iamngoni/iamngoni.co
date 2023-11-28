3e955f56808af7beea407c89572005ec181b7f34
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-11-29 00:07:18 +0200

    fix: index.html

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ngonidzashe Mangudya 👨🏿‍🎓',
       theme: ThemeData(
         // This is the theme of your application.
         //
